0b2bc820a437fae837bf36e9af9e9ddc60d3bf27
Crowdbotics <engineering@crowdbotics.com>
2023-09-15 14:39:14 +0000

    Update store.

--- store/newConnector/api.js
+++ store/newConnector/api.js
@@ -1,11 +1,14 @@
 import axios from "axios"
 import {
-  NEW_CONNECTOR_USERNAME,
-  NEW_CONNECTOR_PASSWORD
+  NEW_CONNECTOR22_PASSWORD,
+  NEW_CONNECTOR22_USERNAME
 } from "react-native-dotenv"
 const newConnector = axios.create({
-  baseURL: "https://sdds.dsd/api",
-  auth: { username: NEW_CONNECTOR_USERNAME, password: NEW_CONNECTOR_PASSWORD },
+  baseURL: "https://sdsd.sd/qpi",
+  auth: {
+    username: NEW_CONNECTOR22_PASSWORD,
+    password: NEW_CONNECTOR22_USERNAME
+  },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 export const apiService = {}
