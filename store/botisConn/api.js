78b56a2be58f977ef3a72b40a02b2b7a9fdbc44c
Crowdbotics <engineering@crowdbotics.com>
2023-09-15 14:31:37 +0000

    Update store.

--- store/botisConn/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-import { BOTIS_CONN_PASSWORD, BOTIS_CONN_USERNAME } from "react-native-dotenv"
-const botisConn = axios.create({
-  baseURL: "https://botis.com",
-  auth: { username: BOTIS_CONN_PASSWORD, password: BOTIS_CONN_USERNAME },
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
